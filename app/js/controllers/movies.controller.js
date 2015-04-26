a179a99f662709c2ecbd19b52ac40ba129c24098
Billy1339 <william.m.sargent@gmail.com>
2015-04-26 16:03:03 -0400

    cleaned the movie titles from movie factory, put in object to make searchable

--- app/js/controllers/movies.controller.js
+++ app/js/controllers/movies.controller.js
@@ -1,14 +1,20 @@
 angular.module('3play').controller('MovieCtrl', function($scope, $http, MoviesFactory) {
   'use strict'
-  debugger
+
   var promise;
 
   promise = MoviesFactory.fetch();
   promise.then(function(movies) {
-    $scope.movies = movies
+    var movieObject, i, movieTitleCleaned;
+    movieObject = {};
+    for (i=0; i < movies.length; i++) {
+      movieTitleCleaned = movies[i].title.toLowerCase().replace(/ /g, "").replace(/'/g,"");
+      movieObject[movieTitleCleaned] = movies[i].id;
+    };
+    $scope.movies = movieObject;
     debugger
   });
-  debugger
+
 
 
 });
