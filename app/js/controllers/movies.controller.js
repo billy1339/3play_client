d6c4183bf7df9fa7280573b6cd1b868ac309ac87
Billy1339 <william.m.sargent@gmail.com>
2015-04-26 17:28:17 -0400

    movie search in working order and making get requests for specific movie data

--- app/js/controllers/movies.controller.js
+++ app/js/controllers/movies.controller.js
@@ -1,7 +1,7 @@
 angular.module('3play').controller('MovieCtrl', function($scope, $http, MoviesFactory) {
   'use strict'
 
-  var promise;
+  var promise, clearMovieForm;
 
   promise = MoviesFactory.fetch();
   promise.then(function(movies) {
@@ -13,6 +13,27 @@ angular.module('3play').controller('MovieCtrl', function($scope, $http, MoviesFa
     };
     $scope.movies = movieObject;
   });
+
+  clearMovieForm = function() {
+    // this removes the title from the scope but does not remove it from the page
+    $scope.movieTitle = {};
+  }
+
+  $scope.enterMovie = function(data) {
+    var cleanedData, movieSearchId;
+    cleanedData = data.toLowerCase().replace(/ /g, "").replace(/'/g,"");
+    movieSearchId = $scope.movies[cleanedData];
+    if (movieSearchId === undefined) {
+      alert("Oops! It looks like you've either mispelled the movie or we dont have the movie :( Please try again!")
+    } else {
+      $http.get('http://localhost:3000/movies/'+movieSearchId).success(function(response) {
+        console.log(response);
+      }).error(function(response){
+        alert(response);
+      });
+    }
+    // clearMovieForm();
+  };
 
 
 
